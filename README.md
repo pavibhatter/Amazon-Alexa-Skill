55255bdfbe40eebb3b94f04b78f9a519d32de6c8
Pavi Bhatter <34725950+pavibhatter@users.noreply.github.com>
2018-12-15 01:39:06 -0700

    added files via upload

---
+++ README.md
@@ -0,0 +1,4 @@
+# Alexa
+StudyBuddy is an Amazon Alexa skill created at the Amazon Future Engineer Intern Hackathon in Seattle, Washington from July 19-20, 2018. Created in partnership with non-profit Code.org, StudyBuddy uses curriculum from Code.org to create test questions that users can quiz themselves with after watching educational videos.<br><br>
+Made by: Pavi Bhatter, Carnegie Mellon University; Jessie Nguyen, University of California, Berkeley; Varun Agarwal, University of California, Berkeley; Prateek Agarwal, University of Pennsylvania
+
---
+++ questions.js
@@ -0,0 +1,72 @@
+module.exports = {
+  /**
+   * When editing your questions pay attention to your punctuation.
+   * Make sure you use question marks.
+   * Make sure the first answer is the correct one.
+   * Set at least ANSWER_COUNT answers, any extras will be shuffled in.
+   */
+  QUESTIONS_EN_US: [
+    {
+      'What is the smallest piece of information a computer can store?': [
+        'Bit',
+        'Code',
+        '5,000',
+      ],
+    },
+    {
+      'How many values can one binary wire represent?': [
+        'Two',
+        'One',
+        'Three',
+        'Four',
+      ],
+    },
+    {
+      'What is the value of the binary number 110 in decimal?': [
+        'Six',
+        'Four',
+        'Two',
+        'Eleven',
+      ],
+    },
+    {
+      'How many values can be represented with four wires?': [
+        'Eight',
+        'Four',
+        'Twelve',
+        'Sixteen',
+      ],
+    },
+    {
+      'How are vibrations represented graphically?': [
+        'Waveforms',
+        'Pixels',
+        'Bit',
+      ],
+    },
+    {
+      'What is the on/off state of a wire called?': [
+        'Bit',
+        'Wave',
+        'Pixel',
+        'Electricity',
+      ],
+    },
+    {
+      'What is the value of a 9 in the 100 position in the decimal number system?': [
+        '900',
+        '9',
+        '100',
+        '90',
+      ],
+    },
+    {
+      'With 8 wires, what is the largest number you can store?': [
+        '255',
+        '0',
+        '32',
+        '64',
+      ],
+    },
+  ],
+};
